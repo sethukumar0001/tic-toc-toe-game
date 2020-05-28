5eb949647b5d85b02ad9ed2b1a5d54cb869b1bef
sethu <sethu@bigappcompany.in>
2020-05-28 22:27:59 +0530

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Square from './Square';
+
+const  Board = ({squares,onClick}) =>(
+  <div className="board">
+      {squares.map((item,i)=>(
+        //   <Square key={i} value={item} onClick={()=>onClick(i)} />
+      ))}
+
+  </div>
+)
+
+export default Board;
